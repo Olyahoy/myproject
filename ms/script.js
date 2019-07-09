9982f38b586f7fb308b3f006da70644e948be305
Alexander <danila@AlexanderIMAC.local>
2019-07-09 14:28:18 +0400

    hw

--- ms/script.js
+++ ms/script.js
@@ -1,1 +1,44 @@
-alert('Hello Mediasoft!');
+function plus() {
+    var n1, n2, result;
+    n1 = document.getElementById('n_1').value;
+    n1 = parseInt(n1);
+
+    n2 = document.getElementById('n_2').value;
+    n2 = parseInt(n2);
+
+    result = n1+n2;
+    document.getElementById("out").innerHTML = result;
+}
+function minus() {
+    var n1, n2, result;
+    n1 = document.getElementById('n_1').value;
+    n1 = parseInt(n1);
+
+    n2 = document.getElementById('n_2').value;
+    n2 = parseInt(n2);
+
+    result = n1-n2;
+    document.getElementById("out").innerHTML = result;
+}
+function multiplication() {
+    var n1, n2, result;
+    n1 = document.getElementById('n_1').value;
+    n1 = parseInt(n1);
+
+    n2 = document.getElementById('n_2').value;
+    n2 = parseInt(n2);
+
+    result = n1*n2;
+    document.getElementById("out").innerHTML = result;
+}
+function division() {
+    var n1, n2, result;
+    n1 = document.getElementById('n_1').value;
+    n1 = parseInt(n1);
+
+    n2 = document.getElementById('n_2').value;
+    n2 = parseInt(n2);
+
+    result = n1/n2;
+    document.getElementById("out").innerHTML = result;
+}
